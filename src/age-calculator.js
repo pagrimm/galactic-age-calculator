b048950f078a9b707c41939817e5f95976fdb3dc
Peter Grimm <pagrimm@gmail.com>
2020-06-26 09:17:57 -0700

    add passing test for creating user object

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,5 +1,7 @@
 export class User {
-  constructor() {
-
+  constructor(name, year, month, day, expectancy) {
+    this.name = name;
+    this.birthday = new Date(year, month, day);
+    this.lifeExpectancy = expectancy;
   }
 }
