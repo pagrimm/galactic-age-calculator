229843be9d8627895f69a6c8f23f74136f8c7faf
Peter Grimm <pagrimm@gmail.com>
2020-06-26 09:14:31 -0700

    add failing test for creating user object

--- __tests__/age-calculator.tests.js
+++ __tests__/age-calculator.tests.js
@@ -1,11 +1,32 @@
-import { Triangle } from './../src/triangle.js';
+import { User } from './../src/age-calculator.js';
 
-describe('Triangle', () => {
+describe('Galactic Age Calculator', () => {
 
-  test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
+  test('should correctly create a user object with name, birthday date object, life expectancy', () => {
+    const user = new User("Peter", 1984, 3, 11, 78);
+    expect(user.name).toEqual("Peter");
+    expect(user.birthday.getDate()).toEqual(11);
+    expect(user.birthday.getFullYear()).toEqual(1984);
+    expect(user.birthday.getMonth()).toEqual(3);
+    expect(user.lifeExpectancy).toEqual(78);
   });
 });
+
+
+/* Creates a user object with user input name, birthday, life expectancy.
+Calculates the number of days user has lived
+Returns user age in Mercury years
+Returns user age in Venus years
+Returns user age in Mars years
+Returns user age in Jupiter years
+Calculates number of days user has left to live based on input life expectancy
+If user is under average life expectancy, returns how many years user has left to live in Mercury years
+If user is over average life expectancy, returns how many years user has lived past the life expectancy in Mercury years
+If user is under average life expectancy, returns how many years user has left to live in Venus years
+If user is over average life expectancy, returns how many years user has lived past the life expectancy in Venus years
+If user is under average life expectancy, returns how many years user has left to live in Mercury years
+If user is over average life expectancy, returns how many years user has lived past the life expectancy in Mercury years
+If user is under average life expectancy, returns how many years user has left to live in Mars years
+If user is over average life expectancy, returns how many years user has lived past the life expectancy in Mars years
+If user is under average life expectancy, returns how many years user has left to live in Jupiter years
+If user is over average life expectancy, returns how many years user has lived past the life expectancy in Jupiter years */
